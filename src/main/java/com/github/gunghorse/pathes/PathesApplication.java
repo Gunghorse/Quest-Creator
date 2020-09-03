239447bf9408451f29579aac1eea40f5304830e5
Dmytro <34006606+Dimonium-239@users.noreply.github.com>
2020-09-03 12:16:20 +0200

    Add files via upload

---
+++ src/main/java/com/github/gunghorse/pathes/PathesApplication.java
@@ -0,0 +1,13 @@
+package com.github.gunghorse.pathes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PathesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/gunghorse/pathes/PathesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gunghorse.pathes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PathesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
